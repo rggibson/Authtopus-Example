a142621edd400bc45b62a90998428a28accfdcd1
Richard Gibson <richard.g.gibson@gmail.com>
2016-05-17 09:31:33 -0700

    Fixed yet another bug where FB.init( ) was failing to be called if FB SDK loaded before angular initialized

--- app/js/authtopusexample.js
+++ app/js/authtopusexample.js
@@ -30,7 +30,19 @@
    */
   function run( $rootScope, $window ) {
     $rootScope.fbInit = false;
-    
+    console.log( "FB not initialized" );
+
+    /* Basic FB auth setup:
+     * https://developers.facebook.com/docs/javascript/quickstart
+     */
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     $window.fbAsyncInit = function( ) {
       FB.init( {
 	appId      : '891867817566349',
@@ -38,6 +50,7 @@
 	version    : 'v2.5'
       } );
 
+      console.log( "FB initialized" );
       $rootScope.$apply( function( ) {
 	$rootScope.fbInit = true;
       } );
